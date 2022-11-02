2c277877c7b7d39cce6faf373fd616e9ad816ea1
remifroger <rem35711@hotmail.fr>
2022-11-02 15:54:32 +0100

    Ajout d'une méthode de secours en JSON - prise en compte des arguments

--- to_csv.py
+++ to_csv.py
@@ -17,13 +17,29 @@ cols = ["col", "alias"]
 rows = []
 
 namespaces = {'gmd': 'http://www.isotc211.org/2005/gmd', 'gco': 'http://www.isotc211.org/2005/gco' }
-tree = Xet.parse('C:\data\metadata\metadata-iso19139.xml')
+### XML method
+tree = Xet.parse(args.file)
 root = tree.getroot()
 nodes = root.findall('gmd:contentInfo/gmd:MD_FeatureCatalogue/gmd:featureCatalogue/gmd:FC_FeatureCatalogue/gmd:featureType/gmd:FC_FeatureType/gmd:carrierOfCharacteristics', namespaces)
 for i in nodes:
-    column_name = i.find('gmd:FC_FeatureAttribute/gmd:memberName', namespaces).text.strip()
-    column_alias = i.find('gmd:FC_FeatureAttribute/gmd:definition/gco:CharacterString', namespaces).text.strip()
+    column_name = i.find('gmd:FC_FeatureAttribute/gmd:memberName', namespaces).text
+    column_alias = i.find('gmd:FC_FeatureAttribute/gmd:definition/gco:CharacterString', namespaces).text
+    rows.append({"col": column_name,
+                 "alias": column_alias })
+df = pd.DataFrame(rows, columns=cols)
+df.to_csv(args.output_name, sep=";", header=True, encoding='utf-8')
+
+"""
+# JSON method
+import xmltodict
+with open('C:\data\metadata\metadata-iso19139.xml', 'r') as f:
+    data = xmltodict.parse(f.read(), encoding='utf-8')
+    dataContent = data['gmd:MD_Metadata']['gmd:contentInfo']['gmd:MD_FeatureCatalogue']['gmd:featureCatalogue']['gmd:FC_FeatureCatalogue']['gmd:featureType']['gmd:FC_FeatureType']['gmd:carrierOfCharacteristics']
+for i in dataContent:
+    column_name = i['gmd:FC_FeatureAttribute']['gmd:memberName']
+    column_alias = i['gmd:FC_FeatureAttribute']['gmd:definition']['gco:CharacterString']
     rows.append({"col": column_name,
                  "alias": column_alias })
 df = pd.DataFrame(rows, columns=cols)
 df.to_csv('C:\data\metadata\metadata.csv', sep=";", header=True, encoding='utf-8')
+"""
